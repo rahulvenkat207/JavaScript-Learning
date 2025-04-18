42098f02ad27ee12d09f631db283a2bc82d2444e
rahulvenkat207 <rahulvenkat207@gmail.com>
2025-04-18 19:28:14 +0530

    objects

---
+++ javascript_variables/5_objects.js
@@ -0,0 +1,12 @@
+let course = {
+    name : "Rahul",
+    subject :"CS"
+};
+
+console.log(course);
+
+course['name'] = "Priya";
+console.log(course)
+
+let property = 'subject';
+console.log(course[property])
