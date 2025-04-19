d0605b755bf530f04f9e2dddaf0ebbb8493116a7
rahulvenkat207 <rahulvenkat207@gmail.com>
2025-04-19 06:41:50 +0530

    second commit

---
+++ controlFlow/4_while.js
@@ -0,0 +1,7 @@
+let numbers = [1,2,3,4,5,6,7]
+let idx = 0
+
+while(idx < numbers.length){
+    console.log(numbers[idx]);
+    idx++;
+}
---
+++ controlFlow/5_doWhile.js
@@ -0,0 +1,6 @@
+let numbers = [1,2,3,4,5,6,7]
+let idx = 0
+
+do{
+    console.log(numbers[idx++]);
+}while(idx < numbers.length)
